9f805a0ea98aa57ccfcef04c019c29fcb062190a
Oliver Johansson <oliverjohansson3@gmail.com>
2021-04-11 11:57:35 +0200

    Added dynamic id

--- server.js
+++ server.js
@@ -2,64 +2,66 @@ const express = require('express')
 const app = express()
 const port = 3000 
 
-const products = [ 
-    {
-        "name": "Dugges The South", 
-        "price": 35,
-        "description": "Fruktig smak med tydlig beska och liten sötma, inslag av apelsinmarmelad, persika, tallbarr, honung, passionsfrukt och grapefrukt.", 
-        "id": 1
-    },
-    {
-        "name": "Dugges New School", 
-        "price": 75,
-        "description": "Nyanserad, rostad, söt smak med inslag av fat, mörk choklad, rostad kokos, kanel, kaffe och soja.",
-        "id": 2
-    }, 
-    {
-        "name": "Dugges Citra", 
-        "price": 25,
-        "description": "Humlearomatisk smak med liten sötma, med inslag av ananas, gula krusbär, sockerkaka och lime.",
-        "id": 3
-    },
-    {
-        "name": "Dugges Bubble Gummy", 
-        "price": 35,
-        "description": "Fruktig smak med tydlig beska, inslag av ananas, nektarin, rosmarin, honung, mango och apelsin.",
-        "id": 4
-    },
-    {
-        "name": "Dugges Cherry", 
-        "price": 32,
-        "description": "Bärig, syrlig smak med tydlig karaktär av körsbär, inslag av mandelmassa och citrus.",
-        "id": 5
-    } 
-]
+const fs = require('fs')
+const data = fs.readFileSync("beer.json")
+let beer = JSON.parse(data)
+
+
 
 app.use(express.static('./public'))
 app.use(express.json())
 
 
 app.post('/api/product', (req, res) => {
-    products.push(req.body)
-    res.status(201).json(req.body)
+    let idToSave = 0
+    beer.forEach((todo) => {
+        if(todo.id > idToSave) {
+            idToSave = todo.id
+        }
+    })
+    idToSave++
+    beer.push({
+        name: req.body.name,
+        price: req.body.price, 
+        description: req.body.description, 
+        id: idToSave
+    })
+    const data = JSON.stringify(beer, null, 2)
+    fs.writeFile("beer.json", data, (err) => {
+        if(err) throw err;
+
+        res.json(beer)
+    })
 })
 
 app.get('/api/product', (req, res) => {
-    res.json(products)
+    res.json(beer)
 })
 
 app.delete('/api/product/:id', (req, res) => {
     const urlID = req.params.id
-    const index = products.findIndex(p => p.id === parseInt(urlID))
-    const deletedProduct = products.splice(index, 1)
-    res.json(deletedProduct)
+    beer = beer.filter((specificBeer) => specificBeer.id !== parseInt(urlID));
+    
+    const data = JSON.stringify(beer, null, 2)
+    fs.writeFile("beer.json", data, (err) => {
+        if(err) throw err;
+        res.json(beer)
+    })
 })
 
 app.put('/api/product/:id', (req, res) => {
+
     const urlID = req.params.id
-    const index = products.findIndex(b => b.id === parseInt(urlID))
-    const newBeer = products.splice(index, 1, req.body)
-    res.json('You changed the beer')
+    const index = beer.findIndex(b => b.id === parseInt(urlID))
+    let specificBeer = beer[index]
+    specificBeer.name = req.body.name
+    specificBeer.price = req.body.price
+    specificBeer.description = req.body.description
+    const data = JSON.stringify(beer, null, 2)
+    fs.writeFile("beer.json", data,(err) => {
+        if(err) throw err
+        res.json('You changed the beer')
+    })
 })
 
 
