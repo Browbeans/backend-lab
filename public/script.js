96bc1ea548a516c8e9d7851e505a79e4233ad497
Oliver Johansson <oliverjohansson3@gmail.com>
2021-04-12 15:52:18 +0200

    Added input fields for changing products

--- public/script.js
+++ public/script.js
@@ -2,21 +2,47 @@ window.addEventListener("load", addEventListeners);
 
 const getAllButton = document.getElementById('getAll')
 const getSpecific = document.getElementById('getSpecific')
-const changeBeer = document.getElementById('changeBeer')
 
 async function addEventListeners() {
   getAllButton.addEventListener('click', showProduct)
-  getSpecific.addEventListener('click', () => makeRequest("/api/product/2", "GET"))
-  changeBeer.addEventListener('click', requestChangeBeer)
+  getSpecific.addEventListener('click', requestSpecificBeer)
 }
 
-async function requestChangeBeer() {
- const body = {
-    name: "Dugges Amarillo", 
-    price: 25,
-    description: "Humlearomatisk smak med tydlig beska, med inslag av ananas, tallbarr, honung, passionsfrukt och apelsinskal."
+
+function createInputsForChange(id) {
+  const container = document.getElementById('container')
+  container.innerHTML = ''
+  const div = document.createElement("div");
+  container.append(div)
+  div.classList.add("box");
+
+  const titleInput = document.createElement('input')
+  const descriptionInput = document.createElement('input')
+  div.append(titleInput)
+  div.append(descriptionInput)
+  let bodyTitle = ''
+  let description = ''
+  titleInput.onchange = () => {
+    bodyTitle = titleInput.value
+    description = descriptionInput.value
+  }
+
+  requestChangeBeer(bodyTitle, description, id)
 }
-  const change = await makeRequest("/api/product/1", "PUT", body)
+
+async function requestChangeBeer(title, description, id) {
+  
+ const body = {
+    name: title, 
+    price: 25,
+    description: description
+}
+  const change = await makeRequest("/api/product/" + id, "PUT", body)
+  console.log(change)
+}
+
+async function requestSpecificBeer() {
+  const change = await makeRequest("/api/product/1", "GET", body)
   console.log(change)
 }
 
@@ -27,18 +53,27 @@ async function showProduct() {
   container.innerHTML = ''
   product.map((productItem) => {
     const div = document.createElement("div");
+    const buttonDiv = document.createElement('div')
     const h3 = document.createElement('h3')
     const pricePara = document.createElement('p')
     const aboutPara = document.createElement('p')
+    const deleteButton = document.createElement('button')
+    const editButton = document.createElement('button')
     h3.innerHTML = productItem.name;
     aboutPara.innerHTML = productItem.description;
     pricePara.innerHTML = productItem.price;
+    deleteButton.innerHTML = 'Ta bort bärsen'
+    editButton.innerHTML = 'Ändra bärsen'
     container.append(div)
     div.classList.add("box");
     div.appendChild(h3);
     div.appendChild(aboutPara);
     div.appendChild(pricePara);
-    div.onclick = async function removeProduct() {
+    div.appendChild(buttonDiv)
+    buttonDiv.appendChild(editButton);
+    buttonDiv.appendChild(deleteButton);
+    editButton.addEventListener('click', () => createInputsForChange(productItem.id)) 
+    deleteButton.onclick = async function removeProduct() {
         fetch("/api/product/" + productItem.id, {
           method: "DELETE",
         })
